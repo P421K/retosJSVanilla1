3b25309dbb58eb1508654bfe05f1ed1460635fe0
P421K <parik4@gmail.com>
2023-10-31 01:02:29 +0100

    retos 1 al 3 hechos. creando readme. organizando un poco las paginas de los retos

--- js/reto3.js
+++ js/reto3.js
@@ -1,9 +1,12 @@
 /*
-****RETO2****
+***RETO 3***
 2. Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida
 por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
 
+3. Modifica la aplicación anterior, para que nos pida el nombre que queremos introducir
+(recuerda usar prompt).
+
 */
 
-let nombre = "Patrick";
+let nombre = prompt("ingresar un nombre");
 console.log(`Bienvenido ${nombre}`);
